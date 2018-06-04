306c18a1fd8b00e893df1d5afaf612b468905be1
DefiledSpec <defiledspec@gmail.com>
2018-06-03 22:19:21 -0500

    finished working game and ui

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,59 @@
 let startNumber, blueVal, redVal, greenVal, yellowVal;
 
+let inputNums;
+let playerScore, playerWins = 0, playerLosses = 0;
 
-let inputNums = [];
-let playerScore;
-
-function updateScore() {
+function updateScore(val) {
 	playerScore = 0;
+	if(val) {
+		inputNums.push(val);
+	}
 	if(inputNums.length >= 1) {
 		for(let i = 0; i < inputNums.length; i++) {
 			playerScore += inputNums[i];
 		}
-		$('#playerScore').text(playerScore);
 	}
-
+	$('#playerScore').text(playerScore);
 }
 
-function display() {
+function checkVal() {
+	console.log(playerScore, startNumber);
+	if (playerScore > startNumber) {
+		console.log('better luck next time.');
+		resetScore();
+		game();
+	} else {
+		if(playerScore < startNumber) {
+			console.log('keep going!');
+		} else if (playerScore === startNumber) {
+			console.log('you win!');
+			updateStreak();
+			game();
+		}
+	}
+}
 
+function resetScore() {
+	playerLosses++;
+	playerScore = 0;
+	$('#losses').text(playerLosses);
+}
+function updateStreak() {
+	playerWins++;
+	playerScore = 0;
+	$('#wins').text(playerWins);
 }
 
 
 function game() {
-	let startNumber = randInt(19, 120);
-	let blueVal = randInt(1, 12);
-	let redVal = randInt(1, 12);
-	let greenVal = randInt(1, 12);
-	let yellowVal = randInt(1, 12);
-	// console.log(blueVal, redVal, greenVal, yellowVal);
-
-
-
+	inputNums = [];
+	updateScore();
+	startNumber = randInt(19, 120);
+	$('#gameNumber').text(startNumber);
+	blueVal = randInt(1, 12);
+	redVal = randInt(1, 12);
+	greenVal = randInt(1, 12);
+	yellowVal = randInt(1, 12);
 }
 
 function randInt(min, max) {
@@ -38,21 +62,18 @@ function randInt(min, max) {
 game();
 
 $('#blue').click(function() {
-
-
-	updateScore();
+	updateScore(blueVal);
+	checkVal();
 });
 $('#red').click(function() {
-
-
-	updateScore();
+	updateScore(redVal);
+	checkVal();
 });
 $('#green').click(function(){
-
-
-	updateScore();
+	updateScore(greenVal);
+	checkVal();
 });
 $('#yellow').click(function() {
-
-	updateScore();
+	updateScore(yellowVal);
+	checkVal();
 });
